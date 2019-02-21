b7e94b1b63c5027de2cda67d82f03a199fdac3f8
mac <scorpion36rus@gmail.com>
2019-02-21 18:55:08 +0300

    DLL was created

---
+++ FrameworkExamples/ConsoleGameFramework/CGF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameFramework
+{
+    public class CGF
+    {
+        public static int ReadInt()
+        {
+            int result;
+            do
+            {
+
+                Console.WriteLine("Введите число");
+
+            } while (!int.TryParse(Console.ReadLine(), out result));
+            return result;
+        }
+    }
+}
--- FrameworkExamples/FrameworkExamples/Program.cs
+++ FrameworkExamples/FrameworkExamples/Program.cs
@@ -3,25 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ConsoleGameFramework;
 namespace FrameworkExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
+           int health = CGF.ReadInt();
         }
 
-        public static int ReadInt()
-        {
-            int result = 0;
-            do
-            {
-                Console.WriteLine("Введите число:");
-            }
-            while (int.TryParse(Console.ReadLine(), out result) == false);
-
-            return result;
-        }
     }
 }
