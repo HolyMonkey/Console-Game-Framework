5dc242469cbd79f1e67732169460e0714b07bacb
negrimez <scorpion36rus@gmail.com>
2019-02-25 00:14:25 +0300

    add Collision class

---
+++ FrameworkExamples/ConsoleGameFramework/Collision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameFramework
+{
+    public class Collision
+    {
+        public static bool Enter(GameObject a, GameObject b)
+        {
+            return a.X == b.X;
+        }
+    }
+}
